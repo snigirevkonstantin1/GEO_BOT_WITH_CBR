edf09d2f9d8e1097dc63027cdb278298396e7d51
Snigirev Konstantin Vladimirov <60485679+snigirevkonstantin1@users.noreply.github.com>
2020-02-27 16:07:11 +0300

    Update openwater.py

--- openwater.py
+++ openwater.py
@@ -1,7 +1,8 @@
 import requests
+from secret_token import WHATER_TOKEN
 
 def whater(latitude, longitude):
-    api_key = '1b565d8876bfa86be8202fa97324dffa'
+    api_key = 'WHATER_TOKEN'
     r = requests.get("http://api.openweathermap.org/data/2.5/weather",
                      params={'lon': longitude, 'lat': latitude, 'type': 'like', 'units': 'metric', 'APPID': api_key})
     mydict = r.json()
